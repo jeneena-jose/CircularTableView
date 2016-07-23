afd8323fc9367365551eaef423ab53d523329bc0
Jeneena Jose <jeneena.jose@indianic.com>
2016-07-23 15:38:37 +0530

    Project initialized

---
+++ CircularTableView/CircularTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CircularTableView
+//
+//  Created by ind563 on 7/23/16.
+//  Copyright © 2016 Indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
